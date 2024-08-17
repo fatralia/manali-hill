46235c79ad1747da84a58eadf9e043419b46fc01
della <fatraayu17@gmail.com>
2024-08-17 18:48:55 +0700

    first commit

---
+++ resources/views/type.blade.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Manali Hill</title>
+
+  <!-- slider stylesheet -->
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+  
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=PT+Serif+Caption&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Text:wght@400;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="css/type.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container pt-3">
+          <a class="navbar-brand" href="index.blade.php">
+            <span>
+              <a href="/">
+                <img src="images/LOGOMANALI1.png" alt="" width="80">
+              </a>
+            </span>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
+              <ul class="navbar-nav  ">
+                <li class="nav-item active">
+                  <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="type"> Type </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="do.html"> Program </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="portfolio.html"> Siteplan </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="portfolio.html"> Facilities </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="portfolio.html"> News </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="contact.html">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+
+  <!-- type -->
+  <section class="type-section">
+    <div class="image-overlay"></div>
+    <div class="content">
+      <h2 class="section-title">Type</h2> <!-- Tambahkan judul di sini -->
+      <div class="card-container">
+        <!-- Card 1 -->
+        <div class="card">
+          <div class="card-image" style="background-image: url('../images/type1.jpg');"></div>
+          <div class="card-content">
+            <div class="sold-tag">SOLD OUT</div>
+            <h3 class="card-title">Green Villa</h3>
+            <p class="card-description">Green Villa merupakan hunian modern <br>
+              dua lantai dari cluster terbaru Green <br>
+              Stone City. Dibangun diatas tanah Seluas <br>
+              60m dengan lantai terbangun seluas 45m. <br>
+              Memiliki 2 kamar tidur dan 1 kamar <br>
+              mandi yang masing-masing terletak pada <br>
+              lantai atas dan lantai bawah bangunan.</p>
+              <p class="card-price"><span class="price-currency">IDR</span> 399.500.000/ Unit</p>
+            <div class="card-images">
+              <div class="card-image-item">
+                <img src="../images/kasur.png" alt="Image 1" class="image1">
+                <span>2</span>
+              </div>
+              <div class="card-image-item">
+                <img src="../images/bath.png" alt="Image 2" class="image2">
+                <span>1</span>
+              </div>
+              <div class="card-image-item">
+                <img src="../images/luas.png" alt="Image 3" class="image3">
+                <span>45</span>
+              </div>
+            </div>
+            <a href="#" class="btn-more-info">Selengkapnya</a>
+          </div>
+        </div>
+        <!-- Card 2 -->
+        <div class="card">
+          <div class="card-image" style="background-image: url('../images/type2.jpeg');"></div>
+          <div class="card-content">
+            <div class="sold-tag">SOLD OUT</div>
+            <h3 class="card-title">Legian</h3>
+            <p class="card-description">Villa Legian merupakan satu satunya <br>
+              rumah villa 2 lantai modern di Kota <br>
+              Malang yang mengusung konsep growing <br>
+              house (rumah tumbuh). Lahan cukup luas <br>
+              yang bisa digunakan sebagai kolam <br>
+              renang, kolam ikan, area barbeque, area <br>
+              bermain anak, gazebo, ataupun taman.</p>
+              <p class="card-price"><span class="price-currency">IDR</span> 574.425.000/ Unit</p>
+            <div class="card-images">
+              <div class="card-image-item">
+                <img src="../images/kasur.png" alt="Image 1" class="image1">
+                <span>4</span>
+              </div>
+              <div class="card-image-item">
+                <img src="../images/bath.png" alt="Image 2" class="image2">
+                <span>4</span>
+              </div>
+              <div class="card-image-item">
+                <img src="../images/luas.png" alt="Image 3" class="image3">
+                <span>45</span>
+              </div>
+            </div>
+            <a href="#" class="btn-more-info">Selengkapnya</a>
+          </div>
+        </div>
+        <!-- Card 3 -->
+        <div class="card">
+          <div class="card-image" style="background-image: url('../images/type3.jpeg');"></div>
+          <div class="card-content">
+            <div class="sold-tag">SOLD OUT</div>
+            <h3 class="card-title">Emerald</h3>
+            <p class="card-description">Tipe Emerald dengan void memiliki 2 luas <br>
+              tanah 60m dengan luas 2 terbangun 50m. <br>
+              Memiliki fasilitas 2 kamar tidur dan 2 <br>
+              kamar mandi. Mengutamakan <br>
+              kenyamanan dengan mengoptimalkan <br>
+              tata ruang yang efisien dan sirkulasi udara <br>
+              yang sehat.</p>
+              <p class="card-price"><span class="price-currency">IDR</span> 599.500.000/ Unit</p>
+            <div class="card-images">
+              <div class="card-image-item">
+                <img src="../images/kasur.png" alt="Image 1" class="image1">
+                <span>2</span>
+              </div>
+              <div class="card-image-item">
+                <img src="../images/bath.png" alt="Image 2" class="image2">
+                <span>1</span>
+              </div>
+              <div class="card-image-item">
+                <img src="../images/luas.png" alt="Image 3" class="image3">
+                <span>60</span>
+              </div>
+            </div>
+            <a href="#" class="btn-more-info">Selengkapnya</a>
+          </div>
+        </div>
+        <!-- Card 4 -->
+        <div class="card">
+          <div class="card-image" style="background-image: url('../images/type4.jpg');"></div>
+          <div class="card-content">
+            <div class="sold-tag">SOLD OUT</div>
+            <h3 class="card-title">Kostel</h3>
+            <p class="card-description">Tipe kostel merupakan pilihan investasi <br>
+              terbaik berupa rumah kost dengan <br>
+              standard hotel. Didesain di atas tanah <br>
+              seluas 60m dengan luas lantai terbangun <br>
+              seluas 67m. Tipe kostel ditunjang dengan <br>
+              fasilitas-fasilitas terbaik demi kenyamanan <br>
+              dan kemudahan aktifitas penghuni.</p>
+              <p class="card-price"><span class="price-currency">IDR</span> 689.500.000/ Unit</p>
+            <div class="card-images">
+              <div class="card-image-item">
+                <img src="../images/kasur.png" alt="Image 1" class="image1">
+                <span>4</span>
+              </div>
+              <div class="card-image-item">
+                <img src="../images/bath.png" alt="Image 2" class="image2">
+                <span>4</span>
+              </div>
+              <div class="card-image-item">
+                <img src="../images/luas.png" alt="Image 3" class="image3">
+                <span>67</span>
+              </div>
+            </div>
+            <a href="#" class="btn-more-info">Selengkapnya</a>
+          </div>
+        </div>
+        <!-- Card 5 -->
+        <div class="card">
+          <div class="card-image" style="background-image: url('../images/type5.jpg');"></div>
+          <div class="card-content">
+            <div class="sold-tag">SOLD OUT</div>
+            <h3 class="card-title">Kostel Exclusive</h3>
+            <p class="card-description">Cluster tipe kostel merupakan pilihan <br>
+              investasi terbaik berupa rumah kost <br>
+              dengan standard hotel. Didesain di atas <br>
+              tanah seluas 60m dengan luas lantai <br>
+              terbangun seluas 67m. Dengan fasilitas-<br>
+              fasilitas terbaik demi kenyamanan dan <br>
+              kemudahan aktifitas <br> penghuni.</p>
+              <p class="card-price"><span class="price-currency">IDR</span> 699.500.000/ Unit</p>
+            <div class="card-images">
+              <div class="card-image-item">
+                <img src="../images/kasur.png" alt="Image 1" class="image1">
+                <span>4</span>
+              </div>
+              <div class="card-image-item">
+                <img src="../images/bath.png" alt="Image 2" class="image2">
+                <span>4</span>
+              </div>
+              <div class="card-image-item">
+                <img src="../images/luas.png" alt="Image 3" class="image3">
+                <span>67</span>
+              </div>
+            </div>
+            <a href="#" class="btn-more-info">Selengkapnya</a>
+          </div>
+        </div>
+        <!-- Card 6 -->
+        <div class="card">
+          <div class="card-image" style="background-image: url('../images/type6.jpg');"></div>
+          <div class="card-content">
+            <div class="sold-tag">SOLD OUT</div>
+            <h3 class="card-title">Villatel</h3>
+            <p class="card-description">Villatel didesain setara dengan hunian <br>
+              berkelas untuk keluarga yang mengusung <br>
+              konsep villa modern 3 lantai. Didukung <br>
+              dengan penataan ruang tamu yang efisien <br>
+              membuat tipe ini menjadi hunian yang <br>
+              nyaman dan hangat untuk keluarga <br>
+              tercinta. Villatel dilengkapi dengan rooftop <br>
+              dilantai tiga untuk kehangatan keluarga.</p>
+              <p class="card-price"><span class="price-currency">IDR</span> 574.425.000/ Unit</p>
+            <div class="card-images">
+              <div class="card-image-item">
+                <img src="../images/kasur.png" alt="Image 1" class="image1">
+                <span>3</span>
+              </div>
+              <div class="card-image-item">
+                <img src="../images/bath.png" alt="Image 2" class="image2">
+                <span>3</span>
+              </div>
+              <div class="card-image-item">
+                <img src="../images/luas.png" alt="Image 3" class="image3">
+                <span>120</span>
+              </div>
+            </div>
+            <a href="#" class="btn-more-info">Selengkapnya</a>
+          </div>
+        </div>
+        <!-- Card 7 -->
+        <div class="card">
+          <div class="card-image" style="background-image: url('../images/type7.jpg');"></div>
+          <div class="card-content">
+            <div class="sold-tag">SOLD OUT</div>
+            <h3 class="card-title">Amethyst</h3>
+            <p class="card-description">Dibangun diatas tanah seluas 193m <br>
+              dengan luas terbangung 150m,Villa <br>
+              Amethyst dilengkapi dengan taman dan <br>
+              kolam renang pribadi. Hunian tipe <br>
+              Amethyst sangat cocok digunakan sebagai <br>
+              aset investasi di lokasi potensial terbaik di <br>
+              Kota Malang serta digunakan sebagai <br>
+              hunian tempat tinggal yang nyaman.</p>
+              <p class="card-price"><span class="price-currency">IDR</span> 599.500.000/ Unit</p>
+            <div class="card-images">
+              <div class="card-image-item">
+                <img src="../images/kasur.png" alt="Image 1" class="image1">
+                <span>4</span>
+              </div>
+              <div class="card-image-item">
+                <img src="../images/bath.png" alt="Image 2" class="image2">
+                <span>4</span>
+              </div>
+              <div class="card-image-item">
+                <img src="../images/luas.png" alt="Image 3" class="image3">
+                <span>193</span>
+              </div>
+            </div>
+            <a href="#" class="btn-more-info">Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  {{-- end --}}
+
+{{-- footer --}}
+<footer class="footer-section">
+  <div class="footer-left">
+      <img src="images/logo pt.png" alt="Logo">
+  </div>
+  <div class="footer-center">
+      <div class="footer-text">
+          <strong>Developed By:</strong>
+          <p>Jl. Sunimbar No.1A, Dawuhan, Jawa Timur,<br>
+            Kec. Karang Ploso, Kabupaten Malang,<br>
+            Jawa Timur 65152</p>
+      </div>
+      <div class="footer-images">
+          <img src="images/wa.png" alt="Image 1">
+          <img src="images/fb1.png" alt="Image 2">
+          <img src="images/ig.png" alt="Image 3">
+          <img src="images/yt.png" alt="Image 4">
+          <img src="images/tt.png" alt="Image 5">
+      </div>
+  </div>
+  <div class="footer-right">
+      <p>Manali Hill | Powered by Sinarindo Digital
+      </p>
+  </div>
+</footer>
+
+{{-- end --}}
+</body>
+
+</html>
